3e9a47cf9ef082cade96c0c58ac819bb83b67ddf
Christian Glacet <cglacet@kune.tech>
2020-05-06 18:40:13 +0200

    Experimental install

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ git_repo=git@github.com:cglacet/uuid-scheme.git
 
 raw_git_url="https://raw.githubusercontent.com/cglacet/install-scripts/master/osx/git-clone.sh?$(date +%s)"
 /bin/bash -c "$(curl -fsSL $raw_git_url)" bash $install_dir $git_repo
+
+cp -r automator/*.workflow ~/Library/Services
+ln -s uuidopen.app ~/Applications/uuidopen.app
