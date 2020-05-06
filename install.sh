1505d85301bab36f70bc07412eb0fb259258ab03
Christian Glacet <cglacet@kune.tech>
2020-05-06 18:37:13 +0200

    Try to have the most recent version of the file

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ install_dir=~/.github/uuid-scheme
 git_repo=git@github.com:cglacet/uuid-scheme.git
 
 raw_git_url="https://raw.githubusercontent.com/cglacet/install-scripts/master/osx/git-clone.sh?$(date +%s)"
-/bin/bash -c "$(curl -fsSL $raw_git_url)" anything $install_dir $git_repo
+/bin/bash -c "$(curl -fsSL $raw_git_url)" bash $install_dir $git_repo
