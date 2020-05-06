2d4d638622a55c2e0e1b3431005ded5eb40d562b
Christian Glacet <cglacet@kune.tech>
2020-05-06 19:00:56 +0200

    Problem in the path

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ git_repo=git@github.com:cglacet/uuid-scheme.git
 raw_git_url="https://raw.githubusercontent.com/cglacet/install-scripts/master/osx/git-clone.sh?$(date +%s)"
 /bin/bash -c "$(curl -fsSL $raw_git_url)" bash $install_dir $git_repo
 
-cp -r automator/*.workflow ~/Library/Services
-ln -s uuidopen.app ~/Applications/uuidopen.app
+cp -r $install_dir/automator/*.workflow ~/Library/Services
+ln -s $install_dir/uuidopen.app ~/Applications/uuidopen.app
